ccad98a5dab7c9d27f7d83b62bae5a03811e3bde
Zalkar <zalkar@bennington.edu>
2019-11-28 21:47:18 -0500

    checking empty image tags in process

--- bin/drone-scripts.js
+++ bin/drone-scripts.js
@@ -1,41 +1,81 @@
 const fs = require('fs');
 const path = require('path');
 
-const test = 'images/api-integrations/Project-ID.png';
+// function walkDir(dir, callback) {
+//     fs.readdirSync(dir).forEach( f => {
+//       let dirPath = path.join(dir, f);
+//       let isDirectory = fs.statSync(dirPath).isDirectory();
+//       isDirectory ? 
+//         walkDir(dirPath, callback) : callback(path.join(dir, f));
+//     });
+//   };
 
-function walkDir(dir, callback) {
-    fs.readdirSync(dir).forEach( f => {
-      let dirPath = path.join(dir, f);
-      let isDirectory = fs.statSync(dirPath).isDirectory();
-      isDirectory ? 
-        walkDir(dirPath, callback) : callback(path.join(dir, f));
-    });
-  };
+// var totalImages = 0;
 
-walkDir('./products', function(filePath) {
-    if (path.extname(filePath) == '.md') {
-        
+// walkDir('./products', function(filePath) {
+//     if (path.extname(filePath) == '.md') {
 
-        var contents = fs.readFileSync(filePath, 'utf8');
+//         var contents = fs.readFileSync(filePath, 'utf8');
 
-        var regex = /(\!\[.+\]\(.+\))/g;
-        var found = contents.match(regex);
+//         var regex = /(\!\[.+\]\(.+\))/g;
+//         var found = contents.match(regex);
 
-        if (found) {
-            console.log("----------");
-            console.log(filePath);
-            console.log("----------");
-            for (var i = 0; i < found.length; i++) {
-                var temp_path = JSON.stringify(found[i], null, 2);
-                var firstIndex = temp_path.indexOf('/images');
-                var new_path = temp_path.substring(firstIndex + 1, temp_path.length - 2);
-                var exists = fs.existsSync(new_path);
-                if (!exists) console.log("Missing image: ", new_path);
-            }
+//         if (found) {
+//             console.log("----------");
+//             console.log(filePath);
+//             console.log("----------");
+//             console.log(found);
+//             totalImages += found.length;
+//             // for (var i = 0; i < found.length; i++) {
+//             //     var temp_path = JSON.stringify(found[i], null, 2);
+//             //     var firstIndex = temp_path.indexOf('/images');
+//             //     var new_path = temp_path.substring(firstIndex + 1, temp_path.length - 2);
+//             //     var exists = fs.existsSync(new_path);
+//             //     if (!exists) console.log("Missing image: ", new_path);
+//             // }
+//         }
+//     }
+// });
+
+// console.log("Images found:", totalImages);
+
+// //Missing image:  "![Screen-Shot-2018-12-03-at-8.31.48-PM.png](https://support.packet.com/file.php/local/303905WCCXWAGDARANNMD0/Screen-Shot-2018-12-03-at-8.31.48-PM.png
+// //![add VLAN jpg](https://raw.githubusercontent.com/packethost/docs/master/images/layer-2-overview/add-vlan.jpg "Add a VLAN")
+
+function checkIfEmpty(pathList) {
+    var emptyPaths = [];
+    for (var i = 0; i < pathList.length; i++) {
+        var currentPath = pathList[i];
+        if (currentPath[1] == '[' && currentPath[2] == ']') {
+            emptyPaths.push(currentPath);
+        } else if (currentPath[currentPath.length - 2] == '(' 
+                    && currentPath[currentPath.length - 1] == ')') {
+            emptyPaths.push(currentPath);
         }
     }
-});
+    return emptyPaths;
+}
+
+function mainImageCheck(filePath) {
+    var contents = fs.readFileSync(filePath, 'utf8');
+    var regex = /(\!\[.*\]\(.*\))/g;
+    var pathList = contents.match(regex);
+
+    // for (var i = 0; i < pathList.length; i++) {
+    //     console.log(pathList[i]);
+    // }
+    // console.log(pathList);
+
+    var emptyPaths = checkIfEmpty(pathList);
+    if (emptyPaths.length > 0) {
+        console.error("The following image tags are incomplete:");
+        console.error(emptyPaths);
+        process.exit(1);
+    }
+
+    console.log("Test completed. All image tags are correct and functional!");
+}
 
 
-//Missing image:  "![Screen-Shot-2018-12-03-at-8.31.48-PM.png](https://support.packet.com/file.php/local/303905WCCXWAGDARANNMD0/Screen-Shot-2018-12-03-at-8.31.48-PM.png
-//![add VLAN jpg](https://raw.githubusercontent.com/packethost/docs/master/images/layer-2-overview/add-vlan.jpg "Add a VLAN")
+mainImageCheck('./test.md');
+
